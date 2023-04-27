f2f9b954b503da228d4430baec7b67fb16a945b4
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2023-04-27 14:01:34 +0200

    setup routes

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HandleClick = () => {
+	
+}
+
+const Login = () => {
+	return (
+		<>
+		<h1>Login</h1>
+		<button>Auth with 42</button>
+		</>
+	)
+}
+
+export default Login;
